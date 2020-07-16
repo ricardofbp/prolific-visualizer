6402a93a693a8c80c91127bd617527f7628c6ff9
ricardofbp <ricardobaiapereira@gmail.com>
2020-07-16 17:07:29 +0100

    basic rewards operations

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ let data = null;
 d3.csv('data.csv', function (d) {
   return {
     title: d['Study'],
+    //TODO parse only 2 decimal cases
     reward: +parseMoney(d['Reward']),
     bonus: +parseMoney(d['Bonus']),
     startTime: parseDate(d['Started At']),
@@ -33,3 +34,51 @@ function parseDate(date) {
   var dateParse = d3.utcParse("%Y-%m-%d %H:%M:%S.%L000");
   return dateParse(date);
 }
+
+//TODO call only once, since static data
+function rewardTotal() {
+  var total = 0;
+  data.forEach(d => {
+    total += +d.reward;
+  });
+  return total;
+}
+//TODO call only once, since static data
+function bonusTotal() {
+  var total = 0;
+  data.forEach(d => {
+    total += +d.bonus;
+  });
+  return total;
+}
+//TODO call only once, since static data
+function totalEarned() {
+  return bonusTotal() + rewardTotal()
+}
+
+function maxReward() {
+  return d3.max(data, d => {
+    return +d.reward;
+  });
+}
+
+function maxBonus() {
+  return d3.max(data, d => {
+    return +d.bonus;
+  })
+}
+
+function minReward() {
+  return d3.min(data, d => {
+    return +d.reward;
+  });
+}
+
+function getMinBonus() {
+  return d3.min(data.filter(d => {
+    return +d.bonus !== 0;
+  }), function (d) {
+    return +d.bonus;
+  });
+}
+
